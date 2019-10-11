d0580f74add623caec9640189a3c2e313b65d0a6
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-10-11 12:33:01 +1000

    Bugfix

--- src/Command/CheckDBSchemaCommand.php
+++ src/Command/CheckDBSchemaCommand.php
@@ -45,7 +45,7 @@ class CheckDBSchemaCommand extends AbstractMoodleCommand
         // EXECUTE COMMAND
         // FREE WIN???
         //$execute = $this->execute;
-        $this->outputHeading($ouput, __DIR__);
+        $this->outputHeading($output, __DIR__);
         $process = new MoodleProcess('check_db_schema.php', $this->moodle->directory.'/admin/cli');
         $code = 0;
         $this->execute->passThroughProcess($process);
