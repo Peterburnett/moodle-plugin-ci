abad5d63053abc5fa36dae68cce1b1996fdd3d5b
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-10-11 12:16:33 +1000

    Changed path

--- src/Command/CheckDBSchemaCommand.php
+++ src/Command/CheckDBSchemaCommand.php
@@ -45,7 +45,7 @@ class CheckDBSchemaCommand extends AbstractMoodleCommand
         // EXECUTE COMMAND
         // FREE WIN???
         //$execute = $this->execute;
-        $process = new MoodleProcess('check_db_schema.php', '../../cli');
+        $process = new MoodleProcess('check_db_schema.php', $this->moodle->directory.'/admin/cli');
         $code = 0;
         $this->execute->passThroughProcess($process);
         if (!$process->isSuccessful()) {
